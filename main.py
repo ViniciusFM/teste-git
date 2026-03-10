fb8a93d19927bed31d52f381b4e9f6c5883a2626
Vinícius F.M. <viniciusmacielf@gmail.com>
2026-03-10 09:34:32 -0300

    fixbug: resolvendo o problema do input

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-n = int(input('Digite um número: '))
-print(n)
+try:
+    n = int(input('Digite um número: '))
+    print(n + 2)
+except ValueError:
+    print('Erro! Você deve entrar com um número')
