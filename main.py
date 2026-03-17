27c42c6f92b8e84f247e8023e8ae96dcfebf6701
Vinícius F.M. <viniciusmacielf@gmail.com>
2026-03-17 08:09:16 -0300

    adicionando a resolução de fatorial iterativo

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+def fatorial(n: int) -> int:
+    '''
+        algoritmo iterativo para resolver fatorial
+        input:
+            n:int - Um valor inteiro qualquer >0
+        output:
+            result - Um valor inteiro >0
+    '''
+    res = 1
+    for i in range(1, n+1):
+        res *= i # mesmo que res = res * i
+    return res
+
 try:
+    print('===== Fatorial =====')
     n = int(input('Digite um número: '))
-    print(n + 2)
+    print(f'Resultado: { fatorial(n) }') # string template
 except ValueError:
     print('Erro! Você deve entrar com um número')
